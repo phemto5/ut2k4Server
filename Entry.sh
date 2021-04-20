fbf80e75a708d6b53d9ad28b8aa0eb2ed4d261bc
Nicholas Anderson <Nicholasaa@gmail.com>
2021-04-20 09:56:45 -0700

    updated to satify prompts

--- Entry.sh
+++ Entry.sh
@@ -5,6 +5,6 @@ echo "./ut2k4server start" | su - ut2k4server
 echo "Printing Details terminal"
 echo "./ut2k4server dt" | su - ut2k4server
 echo "Watching terminal"
-echo "./ut2k4server console" | su - ut2k4server
+printf "./ut2k4server console\nY\n" | su - ut2k4server
 watch uptime
 

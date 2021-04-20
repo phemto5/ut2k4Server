a3e2bb7071a7b02c09c95c3799e6238319677a52
Nicholas Anderson <Nicholasaa@gmail.com>
2021-04-20 09:24:01 -0700

    modified entry

--- Entry.sh
+++ Entry.sh
@@ -2,6 +2,9 @@
 
 echo "Server is Starting"
 echo "./ut2k4server start" | su - ut2k4server
-echo "Watching uptime"
+echo "Printing Details terminal"
+echo "./ut2k4server dt" | su - ut2k4server
+echo "Watching terminal"
+echo "./ut2k4server console" | su - ut2k4server
 watch uptime
 
